832b3f5172461150c3a67d516282db49029798c6
Filip Spiridonov <filip.spiridonov@gmail.com>
2014-05-30 16:40:52 +0300

    Add SQL script with initial data

---
+++ src/test/resources/initialData.sql
@@ -0,0 +1,18 @@
+-- Brand
+INSERT INTO Brand (id, name) VALUES (1, 'Acer');
+INSERT INTO Brand (id, name) VALUES (2, 'Alcatel');
+INSERT INTO Brand (id, name) VALUES (3, 'Apple');
+INSERT INTO Brand (id, name) VALUES (4, 'ASUS');
+INSERT INTO Brand (id, name) VALUES (5, 'Blackberry');
+INSERT INTO Brand (id, name) VALUES (6, 'Fly');
+INSERT INTO Brand (id, name) VALUES (7, 'Gigabyte');
+INSERT INTO Brand (id, name) VALUES (8, 'HTC');
+INSERT INTO Brand (id, name) VALUES (9, 'Huawei');
+INSERT INTO Brand (id, name) VALUES (10, 'Lenovo');
+INSERT INTO Brand (id, name) VALUES (11, 'LG');
+INSERT INTO Brand (id, name) VALUES (12, 'Nokia');
+INSERT INTO Brand (id, name) VALUES (13, 'Philips');
+INSERT INTO Brand (id, name) VALUES (14, 'Samsung');
+INSERT INTO Brand (id, name) VALUES (15, 'Sony');
+INSERT INTO Brand (id, name) VALUES (16, 'SonyEricsson');
+
