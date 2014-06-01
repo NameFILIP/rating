7f3fc600ca704189c4b8818887f2af996b840da1
Filip Spiridonov <filip.spiridonov@gmail.com>
2014-06-01 15:54:24 +0300

    Add queries to Spring Data JPA Repositories

--- src/main/java/org/namefilip/rating/repository/CommentRepository.java
+++ src/main/java/org/namefilip/rating/repository/CommentRepository.java
@@ -1,11 +1,17 @@
 package org.namefilip.rating.repository;
 
+import java.util.List;
+
 import org.namefilip.rating.entity.Comment;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 
 @Transactional(readOnly = true)
 public interface CommentRepository extends CrudRepository<Comment, Long> {
+	
+	@Query("select c from Comment c where c.review.id = ?1")
+	List<Comment> findByReviewId(Long reviewId);
 	
 }
--- src/main/java/org/namefilip/rating/repository/ReviewRepository.java
+++ src/main/java/org/namefilip/rating/repository/ReviewRepository.java
@@ -1,6 +1,9 @@
 package org.namefilip.rating.repository;
 
+import java.util.List;
+
 import org.namefilip.rating.entity.Review;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -8,4 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public interface ReviewRepository extends CrudRepository<Review, Long> {
 	
+	@Query("select r from Review r where r.product.id = ?1")
+	List<Review> findByProductId(Long productId);
 }
